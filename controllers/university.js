78c4216a1fdc3c2fbcb97904cf1ecf6fb10537a3
ahmedhany523 <ahany870@yahoo.com>
2024-02-21 18:22:44 +0800

    updated universities to be filtered based on country choice by user
    updated universities to be filtered based on country choice by user

--- controllers/university.js
+++ controllers/university.js
@@ -14,7 +14,6 @@ exports.getAllUniversities = (req, res) => {
 }
 
 
-const connection = require('../connection'); // Adjust as per your directory structure
 
 exports.getAllUniversities = (req, res) => {
     const query = 'SELECT * FROM university';
@@ -30,19 +29,41 @@ exports.getAllUniversities = (req, res) => {
 }
 
 
-exports.getRandomUniversities = (req, res) => {
+// exports.getRandomUniversities = (req, res) => {
     
-    console.log("Fetching random universities");
-    const query = "SELECT name FROM university ORDER BY RAND() LIMIT 5";
-    connection.query(query, (err, results) => {
+//     console.log("Fetching random universities");
+//     const query = "SELECT name FROM university ORDER BY RAND() LIMIT 5";
+//     connection.query(query, (err, results) => {
+//         if (err) {
+//             console.error("Error fetching random universities:", err);
+//             return res.status(500).json({ error: err.message });
+//         }
+//         console.log("Results:", results);
+//         const universityNames = results.map(uni => uni.name);
+//         console.log("Random Universities: ", universityNames);
+//         res.json(universityNames);
+//     });
+
+// }
+
+
+exports.getRandomUniversities = (req, res) => {
+    const country = req.query.country; // Assuming the country code is passed as a query parameter
+    let query;
+    
+    if (country) {
+        // Make sure to replace 'country' with the actual column name in your table that stores the country information
+        query = "SELECT name FROM university WHERE country = ? ORDER BY RAND() LIMIT 5";
+    } else {
+        query = "SELECT name FROM university ORDER BY RAND() LIMIT 5";
+    }
+    
+    connection.query(query, [country], (err, results) => {
         if (err) {
             console.error("Error fetching random universities:", err);
             return res.status(500).json({ error: err.message });
         }
-        console.log("Results:", results);
         const universityNames = results.map(uni => uni.name);
-        console.log("Random Universities: ", universityNames);
         res.json(universityNames);
     });
-
-}
+};
